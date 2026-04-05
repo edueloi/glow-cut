d61db310be0f0144281b0c755c2c1dd590e10882
Eduardo Eloi Silva Santos <eduardo.santos@comexport.com.br>
2026-04-04 21:01:51 -0300

    Fix: prisma casing e conflito de porta 3001

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { randomUUID } from "crypto";
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
