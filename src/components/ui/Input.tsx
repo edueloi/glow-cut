71520407bca45cbda91396cb7b4b6753df1e072b
Eduardo Eloi Silva Santos <eduardo.santos@comexport.com.br>
2026-05-03 13:21:59 -0300

    fix: virtual_rooms schema completo + join robusto

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -19,7 +19,7 @@ interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "
 }
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-      (
+  (
     {
       label,
       error,
